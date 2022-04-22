89552c9c7ea92a238473bae32100b75815e5a605
hectoralejandrg <enero2169@gmail.com>
2022-04-21 19:22:02 -0500

    configuracion de las variables de entorno

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,14 +5,14 @@ import { MongooseModule } from '@nestjs/mongoose';
 import { Author, AuthorSchema } from './schemas/author.schema';
 import { AuthorController } from './author/author.controller';
 import { AuthorService } from './author/author.service';
+import { ConfigModule } from '@nestjs/config';
 // import { AuthorController } from './author/author.controller';
 // import { AuthorService } from './author/author.service';
 
 @Module({
   imports: [
-    MongooseModule.forRoot(
-      'mongodb+srv://bdbiblioteca:abc123**@cluster0.xewc6.mongodb.net/bdbiblioteca?retryWrites=true&w=majority',
-    ),
+    ConfigModule.forRoot(),
+    MongooseModule.forRoot(`${process.env.DB}`),
     MongooseModule.forFeature([{ name: Author.name, schema: AuthorSchema }]),
   ],
   controllers: [AppController, AuthorController],
